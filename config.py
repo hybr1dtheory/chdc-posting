d37df8be62cf439345abfa6ea69184299f373d87
hybr1dtheory <i.r.galchenko@online.ua>
2024-10-06 00:25:29 +0300

    rm data files

--- config.py
+++
@@ -1,4 +0,0 @@
-"""Local host and target site settings"""
-profile_path = r"C:\Users\INSO_UKR_CENTRALWEST\AppData\Local\Google\Chrome\User Data"
-profile_name = r"Default"
-url = 'https://chdc.ngosafety.org/login'
